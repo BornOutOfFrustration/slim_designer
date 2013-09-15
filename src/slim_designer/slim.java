41b6e24fcc6a02cdc5c685b5ce808820abcc8806
geert jansen <geert.jansen#neorenaissance.nl>
2013-09-15 22:29:24 +0200

    first commit.

---
+++ src/slim_designer/slim.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package slim_designer;
+
+/**
+ * @author geert jansen
+ * @brief starts the program
+ */
+public class slim {
+
+	/**
+	 * @brief main
+	 * @param args parameter that holds startup parameters.
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		return;
+	}
+
+}
